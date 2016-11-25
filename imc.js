d8f719f027ee385b58c25f78b75a4ca2f1470c40
Rivail <menezes.177@hotmail.com>
2016-11-25 00:27:23 -0200

    mensagem

--- imc.js
+++ imc.js
@@ -5,7 +5,15 @@ function calcularIMC() {
 	var centimetros = +formulario.centimetros.value;
 	var altura = (metros * 100 + centimetros)/100;
 	var imc = kilos / (altura * altura);
-     
+   
     formulario.imc.value = imc.toFixed(2);
 
 }
+
+ 
+ 
+
+    
+    
+
+    
---
+++ read-me.mt
@@ -0,0 +1,1 @@
+Programa que calcula o IMC.
